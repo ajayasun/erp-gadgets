ba329bd7d068004d797801e38c4673f69166e9de
venkatesan.ece@gmail.com <venkatesan.ece@gmail.com@60c7dcef-7441-e449-33d8-7df2ba617302>
2011-04-14 16:17:43 +0000

    Changed

--- erp-gadgets/ContextualGadget/src/searchContact.js
+++ erp-gadgets/ContextualGadget/src/searchContact.js
@@ -137,7 +137,7 @@ try
 			debug("Inside searchContactResult method ANSContactNumber",contactId[j].childNodes[0].nodeValue);
 		}
 		var lstName=xmlDoc.getElementsByTagName('LastName');
-		debug("Inside searchContactResult method LastName Node Length value",LastName[j].childNodes.length);
+		debug("Inside searchContactResult method LastName Node Length value",lstName[j].childNodes.length);
 		if(lstName[j].childNodes.length>0)
 		{
 			var cell2 = row.insertCell(1);
@@ -152,7 +152,7 @@ try
 			debug("Inside searchContactResult method LastName",lstName[j].childNodes[0].nodeValue);
 		}
 		var fstName=xmlDoc.getElementsByTagName('FirstName');
-		debug("Inside searchContactResult method FirstName Node Length value",FirstName[j].childNodes.length);
+		debug("Inside searchContactResult method FirstName Node Length value",fstName[j].childNodes.length);
 		if(fstName[j].childNodes.length>0)
 		{
 			var cell3 = row.insertCell(2);
