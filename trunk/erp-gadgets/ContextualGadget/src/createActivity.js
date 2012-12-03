bad64593c45e9d31df6e6548387236e98bcc0e64
venkatesan.ece@gmail.com <venkatesan.ece@gmail.com@60c7dcef-7441-e449-33d8-7df2ba617302>
2012-12-03 07:58:36 +0000

    changed

--- trunk/erp-gadgets/ContextualGadget/src/createActivity.js
+++ trunk/erp-gadgets/ContextualGadget/src/createActivity.js
@@ -272,7 +272,7 @@ try
 		{
 	debug("Inside createActivityResult method Activity Failure with empty text");
 	document.getElementById('content_div').innerHTML = 'Error contacting the server. Please contact your System administrator for support.';
-   QueryActivity();
+  // QueryActivity();
 		}
 	else
 		{
